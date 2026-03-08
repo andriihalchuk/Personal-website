7068a2a985b2570fe4211b46a6e0a52bb56cc13b
andriihalchuk <galhukandrij@gmail.com>
2026-03-08 18:41:48 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-blue-400">
+        Hello, Portfolio!
+      </h1>
+    </div>
+  )
+}
