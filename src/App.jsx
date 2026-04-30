0b0ced9a219214c65b61d31714f6ea7f465e35d2
andriihalchuk <galhukandrij@gmail.com>
2026-04-30 23:16:28 +0100

    navbar buttons and started links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 export default function App() {
   return (
-    <div className ="flex items-center justify-between 
-    bg-gray-800 text-white p-4 text-2xl">
+    <div className="flex items-center justify-around bg-slate-900 text-white p-4 text-2xl">
       <div className="font-bold">Logo</div>
-      <div className="flex gap-4">
-        <span>About Me</span>
-        <span>Projects</span>
-        <span>Skills</span>
-        <span>Contact</span>
+      <div className="flex gap-6">
+        <button className="btn-nav">About Me</button>
+        <button className="btn-nav">Projects</button>
+        <button className="btn-nav">Skills</button>
+        <button className="btn-nav">Contact</button>
       </div>
+      <ul className="flex space-x-3 ">
+          <a href="https://www.linkedin.com/in/andrii-halchuk/">
+          </a>
+      </ul>
     </div>
   );
 }
